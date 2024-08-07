f1eb6a6651fd66c68d6bef3e1f66f010d224ff68
Gonzalo de la Rosa <gonzalitohj@gmail.com>
2024-08-07 02:36:03 +0200

    Test simbolosConsonanticos + estética

--- scripts/general.js
+++ scripts/general.js
@@ -5,3 +5,11 @@ function noDisponible() {
 function prohibido() {
     alert("Todavía no tienes acceso a esta sección");
 }
+
+function goToPage(page) {
+    window.location.href = "/paginas/" + page + ".html";
+}
+
+function goToTest(nombreTest) {
+    window.location.href = "/paginas/test.html?nombre=" + encodeURIComponent(nombreTest);
+}
